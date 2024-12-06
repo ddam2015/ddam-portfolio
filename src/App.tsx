7a75a56f9f04d6f23f562aa8ecacca749835020a
ddam2015 <daradona00@gmail.com>
2024-12-06 13:30:00 -0800

    Debugging deployment 4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DDAM Portfolio.
+          DDAM Portfolio 5
         </p>
         <a
           className="App-link"
